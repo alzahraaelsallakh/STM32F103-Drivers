8cb9f1b4051ff57706db82133f256a6cf2bb9c68
Alzahraa El-Sallakh <zahraaelsallakh@gmail.com>
2020-05-08 07:59:27 +0200

    Uploading Standard Types v1

---
+++ 03-LIB/STD_TYPES.h
@@ -0,0 +1,23 @@
+/************************************************/
+/* Author: Alzahraa Elsallakh                   */
+/* Version: V01                                 */
+/* Date: 19 JAN 2020                            */
+/* Layer: LIB                                   */
+/* Component: STD_TYPES                         */
+/* File Name: STD_TYPES.h                       */
+/************************************************/
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+#define uint8_t  unsigned char
+#define uint16_t unsigned short int
+#define uint32_t unsigned long int 
+
+#define status_t uint32_t
+#define status_Ok  1
+#define status_Nok 2
+
+
+#endif
